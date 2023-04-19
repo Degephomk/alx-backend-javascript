370af5cf5963bec83fcd178a9035ef144a613c17
Degephomk <degiatb.k@gmail.com>
2023-04-19 14:12:21 +0000

    adding

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,25 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  // Add a handler for the promise resolving
-  promise.then((response) => {
-    // Log the fact that the response was received
-    console.log('Got a response from the API');
-
-    // Return an object with the status and body of the response
-    return {
+  return promise
+    .then(() => ({
       status: 200,
-      body: response,
-    };
-  });
-
-  // Add a handler for the promise rejecting
-  promise.catch((error) => {
-    // Log the error
-    console.log(error);
-
-    // Return an empty Error object
-    return {};
-  });
-
-  // Return the promise
-  return promise;
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
