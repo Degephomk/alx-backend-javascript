d2581ff7c85e5b9798376906a3f31f620f4e1a49
Degephomk <degiatb.k@gmail.com>
2023-04-19 13:49:05 +0000

    adding

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,20 @@
+export default function getFullResponseFromAPI(success) {
+  // Create a promise
+  const promise = new Promise((resolve, reject) => {
+    // If success is true, resolve the promise with an object with 2 attributes: status: 200, body: 'Success'
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      // If success is false, reject the promise with an error object with the message The fake API is not working currently
+      reject({
+        message: 'The fake API is not working currently'
+      });
+    }
+  });
+
+  // Return the promise
+  return promise;
+}
