82b61db7a82555d2ea2589c32cdf0877f035df07
Degephomk <degiatb.k@gmail.com>
2023-04-19 14:02:05 +0000

    Adding

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise) {
+  // Add a handler to the promise that logs the success message
+  promise.then(() => {
+    console.log('Got a response from the API');
+  });
+
+  // Add a handler to the promise that returns an object with the following attributes
+  promise.then((response) => {
+    return {
+      status: 200,
+      body: response,
+    };
+  });
