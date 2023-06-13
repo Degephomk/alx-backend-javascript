5db1d989ed836793d7e59f1006d0c40845673ca9
Degephomk <degiatb.k@gmail.com>
2023-06-13 12:27:42 +0000

    adding

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,16 @@
-// 4-http.js
-
 const http = require('http');
+
+const hostname = 'localhost';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-const port = 1245;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
