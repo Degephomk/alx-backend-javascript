95e14b7021db20a54aa6532cd039f75e70b077da
Degephomk <degiatb.k@gmail.com>
2023-06-13 11:49:05 +0000

    adding

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
-function displayMessage(message) {
-  console.log(message); // Print the message to STDOUT
-}
+/**
+ * STDOUT a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+ 
+const displayMessage = (message) => console.log(message);
+module.exports = displayMessage;
